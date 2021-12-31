01d304f51b5dd26646de8cc79d07bb95d73ebe52
shgtkshruch <shgtk.shruch@gmail.com>
2021-12-31 17:53:24 +0900

    Add initial ruby tcp server

---
+++ server.rb
@@ -0,0 +1,32 @@
+require 'socket'
+
+PORT = 1234
+ENDPOINT='0.0.0.0'
+
+server = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+
+sockaddr = Socket.sockaddr_in(PORT, ENDPOINT)
+server.bind(sockaddr)
+
+server.listen(5)
+
+loop do
+  p "Server Start Port: #{PORT}"
+  connection, addr_info = server.accept
+
+  print 'Local address: '
+  p connection.local_address
+
+  print 'Remote address: '
+  p connection.remote_address
+
+  response_body = 'Hello World'
+
+  connection.write("HTTP/1.1 200 OK\r\n" +
+    "Connection: close\r\n" +
+    "\r\n" +
+    response_body
+  )
+
+  connection.close_write
+end
