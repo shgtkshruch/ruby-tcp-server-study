8bfe1082b3944de2fb0e3067a5e592ef51836128
shgtkshruch <shgtk.shruch@gmail.com>
2021-12-31 21:51:22 +0900

    Control middleware by array

--- mock/mock.rb
+++ mock/mock.rb
@@ -4,6 +4,11 @@ require_relative './mime'
 
 module Mock
   PUBLIC_DIR_PATH='./public'
+  Middlewares = [
+    Files,
+    ContentLength,
+    MIME
+  ]
 
   def self.call(connection)
     request_line = connection.gets
@@ -12,9 +17,9 @@ module Mock
     headers = []
     body = ''
 
-    status_line, headers, body = Files.new(request_line).call(status_line, headers, body)
-    status_line, headers, body = ContentLength.new(request_line).call(status_line, headers, body)
-    status_line, headers, body = MIME.new(request_line).call(status_line, headers, body)
+    Middlewares.each do |m|
+      status_line, headers, body = m.new(request_line).call(status_line, headers, body)
+     end
 
     connection.write(status_line + "\r\n" +
       "#{headers.join("\r\n")}" +
