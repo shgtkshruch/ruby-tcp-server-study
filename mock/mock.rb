41976da10deee4c78ce657ab02286a0899bdf636
shgtkshruch <shgtk.shruch@gmail.com>
2022-01-06 22:50:07 +0900

    Add comments

--- mock/mock.rb
+++ mock/mock.rb
@@ -21,6 +21,8 @@ module Mock
       status_line, headers, body = m.new(request_line).call(status_line, headers, body)
      end
 
+    # rfc2616 4.1 Message Types
+    # https://datatracker.ietf.org/doc/html/rfc2616#section-4.1
     connection.write(status_line + "\r\n" +
       "#{headers.join("\r\n")}" +
       "\r\n" +
