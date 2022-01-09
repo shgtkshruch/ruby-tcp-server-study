ee24936aa4ef0c64055962700cff7455bde68b89
shgtkshruch <shgtk.shruch@gmail.com>
2022-01-09 21:36:12 +0900

    cosmetic

--- mock/mock.rb
+++ mock/mock.rb
@@ -27,13 +27,12 @@ module Mock
 
     Middlewares.each do |m|
       status_line, headers, body = m.new(request).call(status_line, headers, body)
-     end
+    end
 
     # rfc2616 4.1 Message Types
     # https://datatracker.ietf.org/doc/html/rfc2616#section-4.1
     connection.write(status_line + "\r\n" +
-      "#{headers.join("\r\n")}" +
-      "\r\n" +
+      "#{headers.join("\r\n")}" + "\r\n" +
       "\r\n" +
       body
     )
